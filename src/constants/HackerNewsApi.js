40797d4a6d9889cf3c11141446f5230eb1313977
Donald Westendorp <don@fullstackdon.com>
2020-02-12 21:51:27 -0600

    Create constants object for Hacker News Api

--- src/constants/HackerNewsApi.js
+++ src/constants/HackerNewsApi.js
@@ -1,4 +1,22 @@
-export const HACKER_NEWS_API_BASE_URL = 'https://hacker-news.firebaseio.com';
-export const HACKER_NEWS_API_VERSION = 'v0';
-export const HACKER_NEWS_TOP_STORIES = 'topstories';
-export const HACKER_NEWS_ASK_STORIES = '';
+/*
+ Constants for defining API endpoints for Hacker News
+ */
+
+class HackerNewsApi {
+
+    BASE_URL = 'https://hacker-news.firebaseio.com';
+    API_VERSION = 'v0';
+    TOP_STORIES = 'topstories';
+    BEST_STORIES = 'beststories';
+    NEW_STORIES = 'newstories';
+    ASK_STORIES = 'askstories';
+    ITEM = 'item';
+    SEPARATOR = '/';
+    EXTENSION = '.json';
+    ENDPOINT_TOP_STORIES = [this.BASE_URL, this.API_VERSION, this.TOP_STORIES].join(this.SEPARATOR);
+    ENDPOINT_BEST_STORIES = [this.BASE_URL, this.API_VERSION, this.BEST_STORIES].join(this.SEPARATOR);
+    ENDPOINT_NEW_STORIES = [this.BASE_URL, this.API_VERSION, this.NEW_STORIES].join(this.SEPARATOR);
+    ENDPOINT_ASK_STORIES = [this.BASE_URL, this.API_VERSION, this.ASK_STORIES].join(this.SEPARATOR);
+    ENDPOINT_ITEM = [this.BASE_URL, this.API_VERSION, this.ITEM].join(this.SEPARATOR);
+
+}
